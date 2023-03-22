ccbcf84468b2d92fb65280dc1b58fc13499400ff
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2023-03-22 20:47:02 +0800

    Added block sections to HomePage
    Added html and some css for HomePage, and altered Footer css to prevent fixed bottom position

--- role_models/src/Pages/HomePage.jsx
+++ role_models/src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-
+import './HomePage.css';
 
 // Components
 import ProfileCard from "../components/ProfileCard/ProfileCard";
@@ -22,6 +22,26 @@ function HomePage() {
         <div className="page-container">
             <h1>Home Page</h1>
             <p>Be the change you want to see.</p>
+            <div className="hero-section">
+                <p>HERO SECTION</p>
+            </div>
+            <div className="stats-section">
+                <div className="stats">
+                    <h3>Stat 1</h3>
+                    <p>Interesting stat</p>
+                </div>
+                <div className="stats">
+                    <h3>Stat 2</h3>
+                    <p>Interesting different stat</p>
+                </div>
+                <div className="stats">
+                    <h3>Stat 3</h3>
+                    <p>Mindblowing stat</p>
+                </div>
+            </div>
+            <div className="profiles-shuffle-board">
+                <p>This is the profiles grid section</p>
+            </div>
 
         </div>
     );
