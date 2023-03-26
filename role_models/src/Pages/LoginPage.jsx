1c76277c688a76e4ebc347d9b2caf0a4bd3a0c82
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2023-03-26 10:21:48 +0800

    Login page created

--- role_models/src/Pages/LoginPage.jsx
+++ role_models/src/Pages/LoginPage.jsx
@@ -1,10 +1,11 @@
+import LoginForm from "../Components/LoginForm/LoginForm.jsx"
+
 function LoginPage() {
 
     return (
         <div className="page-container">
             <h1>Login Page</h1>
-            <p>Login here</p>
-
+            <LoginForm />
         </div>
     );
 }
