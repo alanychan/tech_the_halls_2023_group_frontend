9af9a8b18c4b205344c3ab022cea1721f802d487
what-the-hek <108735805+what-the-hek@users.noreply.github.com>
2023-04-07 10:22:35 +0800

    add hero card form, pages and links

--- role_models/src/Components/CreateHeroForm/CreateHeroForm.jsx
+++ role_models/src/Components/CreateHeroForm/CreateHeroForm.jsx
@@ -1,42 +1,114 @@
-
+import { useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
 
 function CreateHeroForm() {
-    return (
-        <div className="form-container">
-            <form >
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        // onChange={handleChange}
-                        placeholder="Enter first name and last name"
-                    />
-                </div>
-                <div>
-                    <label htmlFor="bio-pic">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        // onChange={handleChange}
-                        placeholder="youremail@email.com"
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        // onChange={handleChange}
-                        placeholder="Password"
-                    />
-                </div>
-                <button type="submit" className="btn">
-                    Create Profile!
-                </button>
-            </form>
-        </div>
-    );
-}
+    const authToken = window.localStorage.getItem("token");
+    const [loggedIn] = useOutletContext();
 
-export default CreateHeroForm;
+    // State
+    const [hero, setHero] = useState({
+      "name": "",
+      "bio-text": "",
+      "bio-pic": "https://images.pexels.com/photos/7985583/pexels-photo-7985583.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "bio-url": "https://images.pexels.com/photos/7985583/pexels-photo-7985583.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "featured": true,
+      "date_created": new Date(),
+    });
+   
+    // Hooks
+    const navigate = useNavigate();
+
+    // Actions
+    const handleChange = (event) => {
+        // target of event is input
+        const {id, value} = event.target;
+
+        // because we're not returning anything, we use ( brackets )
+        setHero((prevHero) => ({
+            // ... take all the values out of this object and put in new object
+            ...prevHero,
+            // this line overrides the previous credentials
+            [id]: value,
+        }));
+    };
+
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (loggedIn) {
+          try {
+            const response = await fetch(
+              `${import.meta.env.VITE_API_URL}hero/`,
+              {
+                method: "post",
+                headers: {
+                  "Content-Type": "application/json",
+                  "Authorization": `Token ${authToken}`,
+                },
+                body: JSON.stringify(hero),
+              }
+            );
+            if (!response.ok) {
+              throw new Error(await response.text());
+            }
+            location.reload();
+          } catch (err) {
+            console.error(err);
+            alert(`Error: ${err.message}`);
+            }
+          } else {
+            // redirect to login page
+            navigate(`/login`);
+          }
+          };
+
+    return (
+      <>
+      {loggedIn?
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            onChange={handleChange}
+            placeholder="Enter their first and last name"
+          />
+        </div>
+        <div>
+          <label htmlFor="bio-text">Short tagline (max 200 characters):</label>
+          <input
+            type="text"
+            id="bio-text"
+            onChange={handleChange}
+            placeholder="Describe their achievements"
+          />
+        </div>
+        <div>
+        <label htmlFor="bio-pic">Profile image:</label>
+          <input
+            type="url"
+            id="bio-pic"
+            onChange={handleChange}
+            placeholder="Enter an image URL"
+          />
+        </div>
+        <div>
+        <label htmlFor="bio-link">Biography:</label>
+          <input
+            type="url"
+            id="bio-link"
+            onChange={handleChange}
+            placeholder="Enter a link to their biography"
+          />
+        </div>
+        <button className="btn" type="submit">
+          Create a hero card
+        </button>
+      </form>
+      : (<p>Log in to create a hero card</p>) }
+      </>
+    );
+  }
+  
+  export default CreateHeroForm;
