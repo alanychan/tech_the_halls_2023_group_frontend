0006e06b2f53710e01c250306c17eeaa91f3f665
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2023-04-09 15:16:54 +0800

    Got questions rendered, Improved CSS for responsiveness
    Successfully grabbing questions/answers with map function
    Refactored code and improved css for responsiveness
    Added mailto for email social and link hover styling for all socials
    Changed video tag to iframe for embedding youtube video successfully at first, but warning now showing in console. Page displaying ok. Unable to use slice() to grab user video url for iframe as of yet

--- role_models/src/Pages/ProfilePage.jsx
+++ role_models/src/Pages/ProfilePage.jsx
@@ -9,13 +9,15 @@ import './ProfilePage.css';
 function ProfilePage(props) {
 
     // Set state
-    const [profile, setProfile] = useState({});
-    // const [questionList, setQuestionList] = useState([]);
-    // const [userAnswers, setUserAnswers] = useState({ user_answers: [] });
     const [loggedIn] = useOutletContext();
+    const [profile, setProfile] = useState({});
+    const [userAnswers, setUserAnswers] = useState([]);
+
 
     // Hooks
     const { id } = useParams();
+
+    // Variables
     const deleteLink = `/delete-profile/${profile.id}`;
     const editLink = `/edit-profile/${id}`;
 
@@ -30,106 +32,111 @@ function ProfilePage(props) {
                 setProfile(data);
             });
 
-        // fetch(`${import.meta.env.VITE_API_URL
-        //     }users-answers`).then((results) => {
-        //         return results.json();
-        //     })
-        //     .then((data) => {
-        //         setUserAnswers(data);
-        //     });
+        fetch(`${import.meta.env.VITE_API_URL
+            }users/${id}`).then((results) => {
+                return results.json();
+            })
+            .then((data) => {
+                setUserAnswers(data.user_answers);
+            });
     }, []);
 
     const profilePic = profile.profile_pic;
-    const video = profile.video;
-    // const answers = profile.answers;
-    const answers = profile.user_answers;
-    // console.log(answers[0].answer);
+    const sendProfileEmail = `mailto:${profile.email}`;
+    const video = `https://www.youtube.com/embed/${profile.video}`;
+    // .slice(17) - can't get this to work on refresh
+    const answers = userAnswers.map(item => {
+        const container = [];
+        container[0] = item.question;
+        container[1] = item.answer;
+
+        return container;
+    })
 
     return (
         <div className="page-container">
+
             {loggedIn &&
                 <div id="edit-delete-container">
-                    <Link to={editLink} className="profile-nav-btns">
+                    <Link to={editLink} className="profile-btns">
                         Edit
                     </Link>
-                    <Link to={deleteLink} className="profile-nav-btns">
+                    <Link to={deleteLink} className="profile-btns">
                         Delete
                     </Link>
                 </div>
             }
-            <div className="column-container">
-                <img src={profilePic} alt="profile picture" className="profile-img" />
+            <div className="columns-container">
+
+                <img src={profilePic} alt="profile picture" className="top-left profile-img" />
+
                 <div className="top-right">
                     <h1>{profile.first_name} {profile.last_name}</h1>
                     <h3 className="profile-h3">{profile.pronouns}</h3>
                     <h3 className="profile-h3">{profile.job_title}</h3>
-                    <h3 className="profile-h3 profile-tagline">{profile.tagline}</h3>
-                    <br></br>
-                    <p className="profile-question">
-                    </p>
+                    <h3 className="profile-tagline">{profile.tagline}</h3>
                 </div>
 
-                <div className="bottom-left">
-                    {/* <ul>
-                        {profile.user_answers.map((answer, key) => {
-                            return <li key={key} answer={answer}>test</li>;
-                        })}
-                    </ul> */}
+                <ul className="bottom-left">
+                    {answers.map((answer, key) => {
+                        return <li key={key}>
+                            <h3 className="profile-h3">
+                                {answer[0]}
+                            </h3>
+                            <p className="profile-answer">
+                                {answer[1]}
+                            </p>
+                        </li>;
+                    })}
+                </ul>
 
-                    <h3 className="profile-h3">What motivates you every day?</h3>
-                    <p className="profile-question">This is the first section - sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis</p>
-                    <br></br>
-                    <h3 className="profile-h3">What inspired you to get into tech?</h3>
-                    <p className="profile-question">{profile?.answers}Sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget felis eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu</p>
-                    <br></br>
-                    <h3 className="profile-h3">What's been your biggest success?</h3>
-                    <p className="profile-question">Viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget felis eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti</p>
-                </div>
+
                 <div className="bottom-right">
                     {profile.video &&
-                        <iframe width="420" height="315" controls>
-                            <source src={video} type="video/mp4" />
-                        </iframe>
+                        <div className="iframe-container">
+                            <iframe width="560" height="315" src={video} title="Profile's YouTube video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
                     }
-                    <div className="profile-contacts">
+
+                    <div className="profile-socials-section">
                         <h2>Get in touch!</h2>
                         <ul>
                             {profile.email &&
                                 <li>
-                                    <h3 className="socials-header">Email: </h3>
+                                    <h3 className="social-header">Email </h3>
                                     <span>
-                                        <a href={profile.email}>
-                                            {profile.email}
+                                        <a href={sendProfileEmail}>Email {profile.first_name}
                                         </a>
                                     </span>
                                 </li>
                             }
                             {profile.blog &&
                                 <li>
-                                    <h3 className="socials-header">Blog: </h3>
+                                    <h3 className="social-header">Blog </h3>
                                     <span>
                                         <a href={profile.blog}>
-                                            {profile.blog}
+                                            Check out {profile.first_name}'s blog
                                         </a>
                                     </span>
                                 </li>
                             }
                             {profile.linkedin &&
                                 <li>
-                                    <h3 className="socials-header">LinkedIn: </h3>
+                                    <h3 className="social-header">LinkedIn </h3>
                                     <span>
+
                                         <a href={profile.linkedin}>
-                                            {profile.linkedin}
+                                            Connect with {profile.first_name} on LinkedIn
                                         </a>
                                     </span>
                                 </li>
                             }
                             {profile.twitter &&
                                 <li>
-                                    <h3 className="socials-header">Twitter: </h3>
+                                    <h3 className="social-header">Twitter </h3>
                                     <span>
                                         <a href={profile.twitter}>
-                                            {profile.twitter}
+                                            Connect with {profile.first_name} on Twitter
                                         </a>
                                     </span>
                                 </li>
@@ -137,9 +144,21 @@ function ProfilePage(props) {
                         </ul>
                     </div>
                 </div>
+
             </div >
         </div >
     );
 }
 
 export default ProfilePage;
+
+
+{/* <h3 className="profile-h3">What motivates you every day?</h3>
+                <p className="profile-question">This is the first section - sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis</p>
+                <br></br>
+                <h3 className="profile-h3">What inspired you to get into tech?</h3>
+                <p className="profile-question">{profile?.answers}Sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget felis eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu</p>
+                <br></br>
+                <h3 className="profile-h3">What's been your biggest success?</h3>
+                <p className="profile-question">Viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget felis eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti</p>
+                </div> */}
